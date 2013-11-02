fb9caff7de6b413b6f01e32fc4d836439800462e
benbaker <bensemailaddress@gmail.com>
2013-11-02 01:01:56 -0500

    cosmetic changes

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -16,13 +16,52 @@
 <br />
 
     <a class="btn btn-link" href='index.php/register'>Create your account</a>
-
     <a class="btn btn-primary " href='index.php/login'>Log in to iDrink</a>
-
     </div>
-
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
 
 <hr>
 
 
-    </div>
+
+  <div class="col-md-4">
+  	<h3>
+    <i class="fa fa-clock-o f-2x text-success "></i>
+    Less thinking.
+    </h3>
+	<p>Stop staring at shelves. Search for the tree wine. Oh, you gave it two stars in November because it gave you a wicked headache. 
+	</p>
+  </div>
+
+
+
+
+  <div class="col-md-4">
+  	<h3>
+    <i class="fa fa-beer fa-2 text-success "></i>
+    More drinking.
+    </h3>
+    <p>You don't remember wether or not you liked that wine you've been staring at on the shelf. Drinky says "omg deliicious" and "headache"
+    </p>
+  </div>
+
+
+
+  <div class="col-md-4">
+  	<h3>
+    <i class="fa fa-envelope a-2x text-success "></i>
+    Easy as %@*#
+    </h3>
+    <p>This app will send you an email the day after you bought the wine and ask you how it was. You can change when you recieve these emails. 
+    </p>
+  </div>
+
+
+
+</div>
